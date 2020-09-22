2bd9689d4c723bf7b0c6d2d438f9d88a6ebf8ad0
vandie <mikevdv@vandie.co.uk>
2020-09-22 21:42:17 +0100

    release: add missing file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Auth } from "./lib/Auth";
+import { Cocktail } from "./lib/Cocktail";
 import { Drink } from "./lib/Drink";
 import { request } from "./lib/Request";
 
-export { Auth, Drink, request };
+export { Auth, Drink, Cocktail, request };
