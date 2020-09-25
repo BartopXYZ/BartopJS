646ec57840151057734761e31313f7162b80dd92
vandie <mikevdv@vandie.co.uk>
2020-09-25 21:34:25 +0100

    release: fix cocktail filtering options

--- src/interfaces/CocktailListOptions.ts
+++ src/interfaces/CocktailListOptions.ts
@@ -2,7 +2,7 @@ import {Drink} from "../lib/Drink";
 
 export interface InterfaceCocktailListOptions {
   drinks?: string[] | Drink[];
-  excludeOtherDrinks?: boolean;
-  allowAlternatives?: boolean;
-  includeDrinkObjects: boolean = true;
+  excludeOtherDrinks?: boolean; // defaults to false
+  allowAlternatives?: boolean; // defaults to false
+  includeDrinkObjects?: boolean; // defaults to true
 }
